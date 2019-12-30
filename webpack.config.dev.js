d075ca79f1634fb97601e08c66da223a94fd3b94
NickoMas <aquinnm@gmail.com>
2019-12-30 14:03:04 +0300

    :wrench: Config changes

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,6 +7,100 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const glob = require('glob');
 
+const clientConfig = function(env, args) {
+  return {
+    mode: env.production ? 'production' : 'development',
+    entry: {
+      ['client/index']: path.resolve(__dirname, 'client/src')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(gsvg|if|png|jpe?g)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: './client/images/[name].[ext]'
+              }
+            }
+          ]  
+        },
+        {
+          test: /\.(mp4)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: './client/videos/[name].[ext]'
+              }
+            }
+          ]  
+        },
+        {
+          test: /\.vue$/,
+          use: ['vue-loader']
+        },
+        {
+          test: /\.js$/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            // 'vue-style-loader',
+            'css-loader'
+          ]
+        },
+        // {
+        //   test: /\.html$/,
+        //   use: [{
+        //     loader: 'html-loader',
+        //     options: {
+        //       minimize: false,
+        //       collapseWhitespace: true
+        //     }
+        //   }]
+        // }
+      ]
+    },
+    // devServer: {
+    //   contentBase: './build',
+    //   hot: true,
+    //   watchOptions: {
+    //     poll: true
+    //   }
+    // },
+    plugins: [
+      // new webpack.HotModuleReplacementPlugin(),
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({
+        filename: `${env.production ? 'client/' : ''}index.html`,
+        template: 'client/src/index.html',
+        inject: false
+      }),
+      new CleanWebpackPlugin({
+        dry: true
+      }),
+      new MiniCssExtractPlugin({
+        filename: `${env.production ? 'client/' : ''}client.css`,
+        chunkFilename: '[id].css'
+      }),      
+
+    ],
+    resolve: {
+      extensions: [ '.vue', '.js' ]
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'build')
+    }
+    // hotUpdateChunkFilename: 'hot/hot-update.js',
+    // hotUpdateMainFilename: 'hot/hot-update.json'
+  }
+};
+
 const serverConfig = function (env, args) {
   return {
     target: 'node',
@@ -32,74 +126,6 @@ const serverConfig = function (env, args) {
   }
 };
 
-const clientConfig = function(env, args) {
-  return {
-    mode: env.production ? 'production' : 'development',
-    entry: {
-      ['client/index']: path.resolve(__dirname, 'client/src')
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(gsvg|if|png|jpe?g)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: './client/images/[name].[ext]'
-              }
-            }
-          ]  
-        },
-        {
-          test: /\.vue$/,
-          use: ['vue-loader']
-        },
-        {
-          test: /\.js$/,
-          use: ['babel-loader']
-        },
-        {
-          test: /\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            // 'vue-style-loader',
-            'css-loader'
-          ]
-        }
-      ]
-    },
-    // devServer: {
-    //   contentBase: './build',
-    //   hot: true,
-    //   watchOptions: {
-    //     poll: true
-    //   }
-    // },
-    plugins: [
-      new CleanWebpackPlugin(),
-      // new webpack.HotModuleReplacementPlugin(),
-      new VueLoaderPlugin(),
-      new HtmlWebpackPlugin({
-        filename: `${env.production ? 'client/' : ''}index.html`,
-        template: 'client/src/index.html',
-        inject: false
-      }),
-      new MiniCssExtractPlugin({
-        filename: `${env.production ? 'client/' : ''}client.css`,
-        chunkFilename: '[id].css'
-      })
-    ],
-    resolve: {
-      extensions: [ '.vue', '.js' ]
-    },
-    output: {
-      filename: '[name].js',
-      path: path.resolve(__dirname, 'build')
-    }
-    // hotUpdateChunkFilename: 'hot/hot-update.js',
-    // hotUpdateMainFilename: 'hot/hot-update.json'
-  }
-};
 
-module.exports = [ serverConfig, clientConfig ];
+module.exports = [ clientConfig, serverConfig ];
+// module.exports = [ serverConfig ];
